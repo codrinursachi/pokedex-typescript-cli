b0ccc5181a4d835172668e4ae9a4a3d38689499e
codrinursachi <codrinursachi@gmail.com>
2025-12-27 19:15:24 +0200

    feat: implement command handling and help functionality in REPL

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 // repl.js actually refers to repl.ts
 import { startREPL } from "./repl.js";
-import * as stream from "stream";
 
 function main() {
   startREPL();
--- src/repl.ts
+++ src/repl.ts
@@ -1,33 +1,52 @@
-import * as readline from 'readline';
-import process from 'process';
+import * as readline from "readline";
+import process from "process";
+import { commandExit } from "./command_exit.js";
+import { commandHelp } from "./command_help.js";
+import { CLICommand } from "./command.js";
 
-export function cleanInput(input: string): string[] { 
+export function cleanInput(input: string): string[] {
     return input
         .toLowerCase()
         .trim()
         .split(/\s+/)
-        .filter(word => word.length > 0);
+        .filter((word) => word.length > 0);
 }
 
-export function startREPL(){
+export function getCommands(): Record<string, CLICommand> {
+    return {
+        exit: {
+            name: "exit",
+            description: "Exits the pokedex",
+            callback: commandExit,
+        },
+        help: {
+            name: "help",
+            description: "Displays help information",
+            callback: commandHelp,
+        },
+    };
+}
+export function startREPL() {
     const rl = readline.createInterface({
         input: process.stdin,
         output: process.stdout,
-        prompt: 'Pokedex > '
+        prompt: "Pokedex > ",
     });
 
     rl.prompt();
 
-    rl.on('line', (line: string) => {
+    rl.on("line", (line: string) => {
         const words = cleanInput(line);
         if (words.length === 0) {
             rl.prompt();
             return;
         }
-        console.log(`Your command was: ${words[0]}`);
+        if (words[0] in getCommands()) {
+            const command = getCommands()[words[0]];
+            command.callback(getCommands());
+        } else {
+            console.log(`Unknown command`);
+        }
         rl.prompt();
-    }).on('close', () => {
-        console.log('Exiting Pokedex REPL. Goodbye!');
-        process.exit(0);
-    });
+    }).on("close", commandExit);
 }

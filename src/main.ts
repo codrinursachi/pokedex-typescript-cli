0a9fab9ead4d06d4ff7d25df57426bee8c741464
codrinursachi <codrinursachi@gmail.com>
2025-12-27 16:50:36 +0200

    feat: enhance REPL functionality and update .gitignore

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,9 @@
+// repl.js actually refers to repl.ts
+import { startREPL } from "./repl.js";
+import * as stream from "stream";
+
 function main() {
-    console.log("Hello, world!");
+  startREPL();
 }
 
 main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,6 +1,33 @@
+import * as readline from 'readline';
+import process from 'process';
+
 export function cleanInput(input: string): string[] { 
     return input
+        .toLowerCase()
         .trim()
         .split(/\s+/)
         .filter(word => word.length > 0);
+}
+
+export function startREPL(){
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        prompt: 'Pokedex > '
+    });
+
+    rl.prompt();
+
+    rl.on('line', (line: string) => {
+        const words = cleanInput(line);
+        if (words.length === 0) {
+            rl.prompt();
+            return;
+        }
+        console.log(`Your command was: ${words[0]}`);
+        rl.prompt();
+    }).on('close', () => {
+        console.log('Exiting Pokedex REPL. Goodbye!');
+        process.exit(0);
+    });
 }
